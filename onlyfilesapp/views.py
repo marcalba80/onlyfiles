dbdecb3e94245a9097e546c1c18514e5b035dd8f
oriol <oriolmiranda15@gmail.com>
2023-11-26 21:30:54 +0100

    add repository views

--- onlyfilesapp/views.py
+++ onlyfilesapp/views.py
@@ -14,3 +14,19 @@ def login(request):
     # return HttpResponse("This is the login page")
     return render(request, 'login.html')
 
+def repository(request):
+
+    # TODO: Don't harcode it, some type of object with the files.
+    context = {
+        "repository": False,
+    }
+    return render(request, 'repository.html', context)
+
+def createRepository(request):
+
+    return render(request, 'createRepository.html')
+
+def addUser(request):
+
+    return render(request, 'addUser.html')
+
